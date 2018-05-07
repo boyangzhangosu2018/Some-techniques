defbf9c2b10b23e444f7f47a36aeb17cf5cf2715
boyangzhangosu <boyangzhangosu@icloud.com>
2018-05-07 15:50:36 -0400

    Update full_model_explianer.R

--- full_model_explianer.R
+++ full_model_explianer.R
@@ -1,3 +1,6 @@
+## This is a protocol code to automately complete full-model selection with 4 variables in the list.
+## Please notice this function doesn't contain lmer function (linear mixed model)
+## Author list: Boyang Zhang (zhang.7077@osu.edu), Yu, Zhongtang, Moraes, Luis
 le <- 4
 p2 <- function (track) {
   if (length(track) == le) {
